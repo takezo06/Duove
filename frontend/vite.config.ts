9a079a731a4018ebbcb3cadd37cb882928f52f2b
takezo06 <@takezo06>
2026-07-10 16:47:18 +0800

    fixed vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,9 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+    build: {
+    cssMinify: false,   // temporarily disable CSS minification
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
